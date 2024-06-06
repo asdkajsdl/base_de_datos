e3ceb60882cb90056f259c83955bb5967d0d98d1
fericos190@gmail.com <emilianomedina357@gmail.com>
2024-06-06 11:16:15 -0300

    hice la tabla de profesores

---
+++ tablas.sql
@@ -0,0 +1,9 @@
+INSERT into profesores (apellido,nombre)
+ VALUES ("Navarro","Andres"),
+		("Mestrovich","Eduardo")
+		("Albornoz",("Jose")
+		("Urso",("Maximiliano")
+		("Villace"),("Federico")
+		("Rovaleti"),("Enrico")
+		("Mayorga"),("Tomas")
+		("Salomon"),("Alejandro");
